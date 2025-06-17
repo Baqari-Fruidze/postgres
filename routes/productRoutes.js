83981ddf35e42e68383622917431ee02945a24ab
Baqari-Fruidze <154324770+Baqari-Fruidze@users.noreply.github.com>
2025-06-18 00:06:30 +0400

    db

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -4,8 +4,9 @@ import {
   getOneProduct,
   updateProduct,
   getCategoryStats,
+  createProduct,
 } from "../controlers/productControler.js";
-import { createProduct } from "../controlers/productControler.js";
+// import { createProduct } from "../controlers/productControler.js";
 import express from "express";
 const router = express.Router();
 
